c062a7bb68842a10a218efc09b217897c1f2519c
e.ghafarzadeh <ghafarzade.elham@gmail.com>
2020-09-29 10:29:29 +0330

    [feature] creating structure of naming server

---
+++ card-management-naming-server/src/main/java/com/samples/cardmanagementnamingserver/CardManagementNamingServerApplication.java
@@ -0,0 +1,13 @@
+package com.samples.cardmanagementnamingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardManagementNamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardManagementNamingServerApplication.class, args);
+	}
+
+}
---
+++ card-management-naming-server/src/test/java/com/samples/cardmanagementnamingserver/CardManagementNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samples.cardmanagementnamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardManagementNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
