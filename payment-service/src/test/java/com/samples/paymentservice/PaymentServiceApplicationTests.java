570536f614f497b0d85f94b93561c00a5b76865e
e.ghafarzadeh <ghafarzade.elham@gmail.com>
2020-09-29 10:31:12 +0330

    [feature] creating structure of payment service

---
+++ payment-service/src/test/java/com/samples/paymentservice/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samples.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
