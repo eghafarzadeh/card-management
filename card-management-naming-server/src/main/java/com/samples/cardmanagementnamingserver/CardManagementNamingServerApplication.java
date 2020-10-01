bb421950d43ee5b4b5364796b007463b79e54893
e.ghafarzadeh <ghafarzade.elham@gmail.com>
2020-10-01 17:26:37 +0330

    [enhancement] add profile to naming server

--- card-management-naming-server/src/main/java/com/samples/cardmanagementnamingserver/CardManagementNamingServerApplication.java
+++ card-management-naming-server/src/main/java/com/samples/cardmanagementnamingserver/CardManagementNamingServerApplication.java
@@ -2,12 +2,16 @@ package com.samples.cardmanagementnamingserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 
 @SpringBootApplication
+@EnableEurekaServer
 public class CardManagementNamingServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CardManagementNamingServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        SpringApplication.run(CardManagementNamingServerApplication.class, args);
+    }
 
 }
